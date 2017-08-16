f4f03ec23c51fb1faf8376f26f71e26fd937da3f
jt222ii <jt222ii@student.lnu.se>
2017-08-16 16:00:28 +0200

    Filter with textfield.
    next: cardbuilder
    
    non-priority todos: css

--- gwent-thing/client/src/Components/CardList.js
+++ gwent-thing/client/src/Components/CardList.js
@@ -2,36 +2,52 @@ import React, { Component } from 'react';
 import Cards from '../cards.json';
 
 class CardList extends Component {
-    render() {
+    constructor() {
+        super();
+        this.state = {
+            search: ''
+        };
+    }
 
+    updateSearch(event) {
+        this.setState({search: event.target.value})
+    }
+
+    render() {
         return(
-        <table>
-            <thead>
-                <td />
-                <td>Name</td>    
-                <td>Power</td>
-                <td>Abilities</td>
-                <td>Faction</td>
-                <td>Row</td>  
-                <td>Type</td>
-            </thead>
-            <tbody>
-                {Cards.map((card, i) => {
-                    return(
-                    <tr key={i}>
-                        <td>
-                            <img src={card.imageurl} alt="" id="cardImage" />
-                        </td>
-                        <td>{card.Name}</td>    
-                        <td>{card.Power}</td>
-                        <td>{card.Abilities}</td>
-                        <td>{card.Faction}</td>
-                        <td>{card.Row}</td>  
-                        <td>{card.Type}</td>
-                    </tr>);
-                })}
-            </tbody>
-        </table>
+            <div>
+                <input id="searchBox" type="text" name="search" value={this.state.search} onChange={this.updateSearch.bind(this)}/>
+                <table id="cardTable">
+                    <thead>
+                        <td></td> 
+                        <td>Name</td>    
+                        <td>Power</td>
+                        <td>Abilities</td>
+                        <td>Faction</td>
+                        <td>Row</td>  
+                        <td>Type</td>
+                    </thead>
+                    <tbody>
+                        {
+                            Cards.filter((card) => card.Name.toLowerCase().match('^'+this.state.search.toLowerCase()+'.*'))
+                                .map((card, i) => {
+                                    return(
+                                    <tr key={i}>
+                                        <td>
+                                            <img src={card.imageurl} alt="" id="cardImage" />
+                                        </td>
+                                        <td>{card.Name}</td>    
+                                        <td>{card.Power}</td>
+                                        <td>{card.Abilities}</td>
+                                        <td>{card.Faction}</td>
+                                        <td>{card.Row}</td>  
+                                        <td>{card.Type}</td>
+                                    </tr>);
+                                })
+                            }
+                    </tbody>
+                </table>
+            </div>
         )
     }
 }
