40bff840246415d5bc62e660d44fd647d0f81a45
jt222ii <jt222ii@student.lnu.se>
2017-08-19 15:43:47 +0200

    You can now add and remove cards to/from your deck. Can not save the deck yet though and there are some minor fixes that need to be made when changing faction.

--- gwent-thing/client/src/Components/DeckBuilder.js
+++ gwent-thing/client/src/Components/DeckBuilder.js
@@ -6,16 +6,65 @@ class DeckBuilder extends Component {
         super();
         this.state = {
             search: '',
-            pickedFaction: '-'
+            pickedFaction: '-',
+            currentDeck: [],
+            bronzeAmount: 0,
+            silverAmount: 0,
+            goldAmount: 0
         };
     }
 
+    //update state for filter "search"
     updateSearch(event) {
         this.setState({search: event.target.value})
     }
 
+    //Update state for filter "pickedFaction"
     setFaction(event) {
         this.setState({pickedFaction: event.target.value})
+    }
+
+    //Adds a card to the deck
+    addCard(card) {
+        if(card.Type === 'Bronze') {
+            if(this.cardCount(card) >= 3) {
+                return;
+            }
+        } else {
+            if((card.Type === 'Silver' && this.state.silverAmount >= 6) || (card.Type === 'Gold' && this.state.goldAmount >= 4) || this.cardCount(card) >= 1) {
+                return;
+            }
+        }
+
+        if(card.Type === 'Bronze') {
+            this.state.bronzeAmount++;
+        } else if(card.Type === 'Silver') {
+            this.state.silverAmount++;
+        } else {
+            this.state.goldAmount++;
+        }
+
+        //Adds the new card, also sorts the array for readability purposes
+        this.setState({currentDeck:[...this.state.currentDeck, card].sort((a,b) => a.Name.localeCompare(b.Name))}); 
+        
+    }
+
+    removeCard(card) {
+        let array = this.state.currentDeck;
+        array.splice(array.indexOf(card), 1);; 
+        this.setState({currentDeck: array });
+        if(card.Type === 'Bronze') {
+            this.state.bronzeAmount--;
+        } else if(card.Type === 'Silver') {
+            this.state.silverAmount--;
+        } else {
+            this.state.goldAmount--;
+        }
+    }
+
+    //returns the amount of the specific card that is currently in the deck
+    cardCount(card) {
+        return this.state.currentDeck.reduce((n, val) => {return n + (val === card);}, 0);
     }
 
     render() {
@@ -40,8 +89,9 @@ class DeckBuilder extends Component {
                     <ul id="cardPickerList">
                         {
                             Cards.filter((card) => card.Name.toLowerCase().match('^'+this.state.search.toLowerCase()+'.*'))
-                                .filter((card) => card.Faction.toLowerCase().match('^'+this.state.pickedFaction.toLowerCase()+'.*'))
-                                .map((card, i) => {
+                                .filter((card) => card.Faction.toLowerCase().match('^'+this.state.pickedFaction.toLowerCase()+'|neutral.*'))
+                                .filter((card) => card.Type.toLowerCase() !== 'leader')
+                                .map((card, i) => {     
                                     let maxAmount = 3;
                                     if(card.Type !== 'Bronze')
                                     {
@@ -53,7 +103,13 @@ class DeckBuilder extends Component {
                                                 <div>{card.Name}</div>
                                                 <div>P: {card.Power}</div>
                                                 <div>Type: {card.Type}</div>
-                                                <div>0/{maxAmount}  +</div>
+                                                <div>
+                                                    <div onClick={
+                                                        ()=> {
+                                                            this.addCard(card)
+                                                        }
+                                                    }>+({this.cardCount(card)}/{maxAmount})</div>
+                                                </div>
                                             </div>
                                         </li>
                                     );
@@ -62,7 +118,29 @@ class DeckBuilder extends Component {
                     </ul>
                 </div>
                 <div id="CardPicker">
-                        <p>THE DECK BUILT WILL BE DISPLAYED HERE</p>
+                        <ul id="cardPickerList">
+                        {
+                            this.state.currentDeck.filter((card) => card.Name.toLowerCase().match('^'+this.state.search.toLowerCase()+'.*'))
+                                .map((card, i) => {     
+                                    return(
+                                        <li key={i} id="CardPickerItem">
+                                            <div>
+                                                <div>{card.Name}</div>
+                                                <div>P: {card.Power}</div>
+                                                <div>Type: {card.Type}</div>
+                                                <div>
+                                                    <div onClick={
+                                                        ()=> {
+                                                            this.removeCard(card)
+                                                        }
+                                                    }>-</div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    );
+                                })
+                        }
+                    </ul>
                 </div>
             </div>
         )
